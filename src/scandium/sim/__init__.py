347528e26aa3ddcade6bdd5853c27ab0649a1642
Emre <emrevaltn@gmail.com>
2026-08-16 15:50:38 +0300

    Delete src/scandium/sim directory

--- src/scandium/sim/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Simulation module for Scandium."""
-
-from scandium.sim.airsim_bridge import AirSimBridge
-from scandium.sim.scenario_runner import ScenarioRunner
-
-__all__ = [
-    "AirSimBridge",
-    "ScenarioRunner",
-]
